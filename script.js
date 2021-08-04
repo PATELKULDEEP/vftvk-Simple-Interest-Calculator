4878a27d1abb0db4eacd76a9cbc7f18cde848e58
KULDEEP <39936050+PATELKULDEEP@users.noreply.github.com>
2021-08-04 17:00:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-function compute() {
-    //Get the values and calculate 
-    var principal = parseFloat(document.getElementById("principal").value);
-    var rate = parseFloat(document.getElementById("rate").value);
-    var years = parseInt(document.getElementById("years").value);
-    var interest = principal * years * rate / 100;
-    var yearInTheFuture = new Date().getFullYear() + years;
-    //Create the Interest text
-    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
-        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
-        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
-        "in the year <mark>" + yearInTheFuture + "</mark>";
-
-}
-
-//update the rate value
-function getSliderValue() {
-    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
-}
-
-//Check for positive values
-function validateAmount() {
-    var principal = document.getElementById("principal").value;
-    var biggerThanZero = parseInt(principal) > 0;
-    if (!biggerThanZero) {
+ 
+function compute()
+{
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest  = principal * years * rate / 100;
+    let actualYear =  new Date().getFullYear();
+    let futureyear = Number(years) + actualYear;  
+    if (principal <= 0)
+    {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
     }
+    else 
+    {
 
+    
+    let s = "If you deposit <span class='highlight' >"+ String(principal)+
+            ",</span><br> at an interest rate of <span class='highlight' >"+ String(rate)+
+            "%</span>.<br>You will receive an amount of <span class='highlight' >"+String(interest)
+            +"</span>,<br> in the year <span class='highlight' >"+ String(futureyear)+"</span>";
+            
+     document.getElementById("result").innerHTML = s;       
+     }
+    
 }
+
+function displayRange(){
+    let rate = document.getElementById("rate").value;
+    document.getElementById("span_range").innerHTML = rate;
+}
+
